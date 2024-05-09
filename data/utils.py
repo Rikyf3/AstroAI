a7cf9da0636a6129d42e4dc7103d88f3c6504221
Riccardo Alberghi <alberghir@gmail.com>
2024-05-09 14:27:26 +0200

    Added linear normalization

--- data/utils.py
+++ data/utils.py
@@ -1,10 +1,11 @@
 import torch
-import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 
 
 def median_abs_deviation(x, median_x):
-    x = torch.median(torch.abs(x - median_x))
+    x = torch.abs(x - median_x)
+    x = torch.median(x.reshape(3, -1), dim=1).values[:, None, None]
     return x
 
 
@@ -13,7 +14,7 @@ def stretch(x, normalize=False):
         med = torch.median(x)
         x = (x - med) / median_abs_deviation(x, med) * 0.04
 
-    return torch.clip(x.permute(1, 2, 0) * 3 + 0.2, 0.0, 1.0)
+    return torch.clip(x.permute(1, 2, 0) * 3 + 0.3, 0.0, 1.0)
 
 
 @torch.no_grad()
@@ -21,14 +22,15 @@ def plot_batch(image, output, bkg, epoch, num_of_images=8):
     fig, axs = plt.subplots(num_of_images, 3, figsize=(16.5, 30.5))
 
     for i in range(num_of_images):
-        axs[i, 0].imshow(stretch(image[i].cpu(), normalize=True), interpolation="lanczos")
-        axs[i, 1].imshow(stretch(output[i].cpu(), normalize=True), interpolation="bilinear")
-        axs[i, 2].imshow(stretch(bkg[i].cpu(), normalize=True), interpolation="bilinear")
+        axs[i, 0].imshow(stretch(image[i].cpu(), normalize=False), interpolation="lanczos")
+        axs[i, 1].imshow(stretch(output[i].cpu(), normalize=False), interpolation="bilinear")
+        axs[i, 2].imshow(stretch(bkg[i].cpu(), normalize=False), interpolation="bilinear")
 
     fig.savefig(f"plot_{epoch}", bbox_inches='tight')
     plt.close(fig)
 
 
+# TODO : make it channel-wise
 def log_norm(x, min_=None, mean_=None, std_=None, epsilon=1e-5, clip=False):
     min_ = torch.min(x) if min_ is None else min_
 
@@ -52,13 +54,29 @@ def log_denorm(x, min_, mean_, std_, epsilon=1e-5):
     return x
 
 
-@torch.compile()
+def lin_norm(x, med_=None, mad_=None):
+    med_ = torch.median(x.reshape(3, -1), dim=1).values[:, None, None] if med_ is None else med_
+    mad_ = median_abs_deviation(x, med_) if mad_ is None else mad_
+
+    x = (x - med_) / mad_ * 0.04
+    x = torch.clip(x, -10, 10)
+
+    return x, med_, mad_
+
+
+def lin_denorm(x, med_, mad_):
+    return (x + med_) * mad_ / 0.04
+
+
 def linear_fit(x, y, clipping=0.95):
     x, y = x.numpy(), y.numpy()
 
+    def f(x_in, slope, intercept):
+        return x_in * slope + intercept
+
     for c in range(x.shape[0]):
-        indx_clipped = x[c, :, :].flatten() < clipping * x.max()
-        coeff = np.polyfit(y[c, :, :].flatten()[indx_clipped], x[c, :, :].flatten()[indx_clipped], 1)
-        y[c, :, :] = y[c, :, :] * coeff[0] + coeff[1]
+        indx_clipped = x[c, :, :].flatten() < clipping
+        popt, _ = scipy.optimize.curve_fit(f, y[c, :, :].flatten()[indx_clipped], x[c, :, :].flatten()[indx_clipped], p0=(1, 0), method='lm')
+        y[c, :, :] = y[c, :, :] * popt[0] + popt[1]
 
     return y
